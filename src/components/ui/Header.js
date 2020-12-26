72a717eb397bf5777992b6d906999005cb734d60
Iyad <iyadhb@gmail.com>
2020-12-26 16:52:11 +0000

    Header setup

---
+++ src/components/ui/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+
+const Header = () => {
+    return (
+       <AppBar>
+        <Toolbar>
+        Arc Development
+        </Toolbar>
+       </AppBar>
+    )
+}
+
+export default Header
