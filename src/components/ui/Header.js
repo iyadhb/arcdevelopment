1c14f6113c4a3f128ba02638858c9135dbaeddd0
Iyad <iyadhb@gmail.com>
2020-12-26 22:12:11 +0000

    Active Tap Refresh fix and logo click

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
@@ -30,7 +30,13 @@ const useStyles = makeStyles(theme => ({
         marginBottom: "3em"
     },
     logo: {
-       height: '7em' 
+       height: '8em' 
+    },
+    logoContainer: {
+        padding: 0,
+        "&:hover": {
+            backgroundColor: "transparent"
+        }
     },
     tabContainer: {
         marginLeft: 'auto',
@@ -57,13 +63,35 @@ const Header = (props) => {
     const handelChange = (e, value) => {
         setValue(value)
     }
+
+    useEffect(() => {
+        if (window.location.pathname === "/" && value !== 0){
+            setValue(0) 
+        } else if (window.location.pathname === "/services" && value !== 1){
+            setValue(1)
+        } else if (window.location.pathname === "/revolution" && value !== 2){
+            setValue(2)
+        } else if (window.location.pathname === "/about" && value !== 3){
+            setValue(3)
+        } else if (window.location.pathname === "/contact" && value !== 4){
+            setValue(4)
+        } else if (window.location.pathname === "/estimate" && value !== 5){
+            setValue(5)
+        }
+           
+    }, [value])
     return (
         <React.Fragment>
             <ElevationScroll>
                 <AppBar position="fixed" color='primary'>
                 <Toolbar disableGutters>
+                <Button component={Link} to="/" 
+                disableRipple
+                className={classes.logoContainer} onClick={() => setValue(0)}>
+                    <img src={logo} alt="Company Logo" className={classes.logo} />
+                </Button>
                 
-                <img src={logo} alt="Company Logo" className={classes.logo} />
+
                 <Tabs value={value} onChange={handelChange} indicatorColor="primary" className={classes.tabContainer}>
                     <Tab className={classes.tab} component={Link} to="/" label="Home" />
                     <Tab className={classes.tab} component={Link} to="/services" label="Services" />
